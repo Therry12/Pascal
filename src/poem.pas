821264771ef801212030c539c8357faaaab57510
Therry12 <incompsince@gmail.com>
2024-12-01 20:14:21 +0300

    Added homeworke: poem

---
+++ src/poem.pas
@@ -0,0 +1,18 @@
+program poem_text;
+
+begin
+    write('Sun of the sleepless! Melancholy star!');
+    readln;
+    write('That show’st the darkness thou canst not dispel,');
+    readln;
+    write('How like art thou to joy remember’d well!');
+    readln;
+    writeln('So gleams the past, the light of other days,');
+    readln;
+    write('Which shines, but warms not with its powerless rays');
+    readln;
+    write('A night-beam Sorrow watcheth to behold,');
+    readln;
+    writeln('Distinct, but distant — clear, but oh, how cold!');
+    
+end.
