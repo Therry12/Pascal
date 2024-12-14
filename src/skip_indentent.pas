bdeb4073d10e8186c4d4096092ee6811868f41d9
Therry12 <incompsince@gmail.com>
2024-12-14 19:40:35 +0300

    fit: Added skip_indentent.pas, that prints lines without intendent.

---
+++ src/skip_indentent.pas
@@ -0,0 +1,30 @@
+program skip_indentent.pas;
+
+var
+    know, print: boolean;
+    c: char;
+
+begin
+    print := false;
+    know := false;
+    while not eof do
+    begin  
+        read(c);
+        if c = #10 then 
+        begin
+            if know and print then
+                writeln;
+            know := false
+        end
+        else
+        begin
+            if not know then
+            begin
+                print := (c <> ' ') and  (c <> #9);
+                know := true
+            end
+        end;
+        if print then
+            write(c);
+    end
+end.
