41788ee86f1afcaa79b782c8390f2b57fb179e2f
Daniel <incompsince@gmail.com>
2024-12-23 20:57:06 +0300

    fit: practice with ReadKey function

---
+++ src/read_key.pas
@@ -0,0 +1,16 @@
+program read_key;
+uses crt;
+
+var
+  c, cc: char;
+
+begin
+  repeat 
+    c := ReadKey;
+    cc := c;
+    if (cc < #32) or (cc > #126) then
+        cc := '?';
+    writeln(ord(c), ' (', cc, ')')
+    until c = ' '
+end.
+
